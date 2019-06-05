7b755d662395a17238f80be167bda5a900f240ae
palexander <Paxton.Alexander@sdl.usu.edu>
2019-06-05 17:22:14 -0600

    Update ask function, add examples

--- template.sh
+++ template.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 ##################################################################################################
-# Author: Paxton Alexander
+# Author: Paxton Alexander (With the help of Github and StackOverflow)
 # Version: 1.0
 # Description: A template to be used for quick creation of bash scripts.
 ##################################################################################################
@@ -46,19 +46,40 @@ log() { # Args: log_level[0-4], Message
     fi
 }
 
-get_input() { # args: question
+# https://gist.github.com/davejamesmiller/1965569
+ask() {
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
     while true; do
-        echo "Continue [y/n]?"
-        read -n1 -s
-        case "$REPLY" in
-            [yY]*)
-                return 0 ;;
-            [nN]*) 
-                return 1 ;;
-            *)
-                echo "Invalid input..." ;;
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
+
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
         esac
-done
+    done
 }
 
 menu() 
@@ -120,3 +141,40 @@ log 1 "Error"
 log 2 "Warn"
 log 3 "Info"
 log 4 "Debug"
+
+# Ask() Examples
+if ask "Do you want to do such-and-such?"; then
+    echo "Yes"
+else
+    echo "No"
+fi
+
+# Default to Yes if the user presses enter without giving an answer:
+if ask "Do you want to do such-and-such?" Y; then
+    echo "Yes"
+else
+    echo "No"
+fi
+
+
+# Default to No if the user presses enter without giving an answer:
+if ask "Do you want to do such-and-such?" N; then
+    echo "Yes"
+else
+    echo "No"
+fi
+
+# Only do something if you say Yes
+if ask "Do you want to do such-and-such?"; then
+    echo "yes"
+fi
+
+# Only do something if you say No
+if ! ask "Do you want to do such-and-such?"; then
+    echo "no"
+fi
+
+# Or if you prefer the shorter version:
+ask "Do you want to do such-and-such?" && echo "yes"
+
+ask "Do you want to do such-and-such?" || echo "no"
